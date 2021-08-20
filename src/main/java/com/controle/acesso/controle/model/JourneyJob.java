0116186ce1611c68b5f7426d555162cb98c687d8
Felipe D. Santos <lycan.felipe@gmail.com>
2021-08-19 22:41:31 -0300

    Created Class JourneyJob with anotations lomboton

---
+++ src/main/java/com/controle/acesso/controle/model/JourneyJob.java
@@ -0,0 +1,14 @@
+package com.controle.acesso.controle.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JourneyJob {
+    private long id;
+    private String descricao;
+}
