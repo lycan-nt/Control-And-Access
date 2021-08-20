6fa029134d820176ef4c4748b56863579788f142
Felipe D. Santos <lycan.felipe@gmail.com>
2021-08-20 06:05:42 -0300

    Created class Moviment

--- src/main/java/com/controle/acesso/controle/model/Calendar.java
+++ src/main/java/com/controle/acesso/controle/model/Calendar.java
@@ -1,4 +1,18 @@
 package com.controle.acesso.controle.model;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
 public class Calendar {
+    private long id;
+    private DateType dateType;
+    private String description;
+    private LocalDateTime specialDate;
 }
